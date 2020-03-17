c1bbae4276c3b18533ddd9fc066fc133c9c29496
James <auminist3@gmail.com>
2020-03-18 09:51:09 +1200

    Updates

---
+++ precache-manifest.3d055efe1f660cebd28dad0e4e5895b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a7bd020df7be864170be20d19a6d09",
+    "url": "/coaxiom/index.html"
+  },
+  {
+    "revision": "cead4114c20f1901c090",
+    "url": "/coaxiom/static/css/main.62af85bd.chunk.css"
+  },
+  {
+    "revision": "9c3f7f62b65375a0edce",
+    "url": "/coaxiom/static/js/2.f77bad7b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/coaxiom/static/js/2.f77bad7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cead4114c20f1901c090",
+    "url": "/coaxiom/static/js/main.9773de8a.chunk.js"
+  },
+  {
+    "revision": "50b561c213cee92d50da",
+    "url": "/coaxiom/static/js/runtime-main.ec98a8c1.js"
+  },
+  {
+    "revision": "612398806ff9d0ae125de8b5a658f30a",
+    "url": "/coaxiom/static/media/business1.61239880.jpg"
+  },
+  {
+    "revision": "1680a27a6e6a42cf8abfe5a15c8b2e83",
+    "url": "/coaxiom/static/media/consulting1.1680a27a.jpg"
+  },
+  {
+    "revision": "59005cd4ea6c93c36093010ad5e3fb21",
+    "url": "/coaxiom/static/media/risk1.59005cd4.jpg"
+  }
+]);
